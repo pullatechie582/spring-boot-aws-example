3b3d10747af99a059664e908652f41cfce5ea52e
pullatechie582 <pullarao.kanchupaty582@gmail.com>
2021-06-02 11:51:54 +0530

    first commit

---
+++ src/test/java/com/javatechie/saws/api/SpringBootAwsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.saws.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
